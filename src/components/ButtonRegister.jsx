df52603c209077e116f9f3e5fd24c35f2a65024c
DianaMS <dmartinezs@uni.pe>
2020-08-21 17:55:48 -0500

    componentes boton Register

---
+++ src/components/ButtonRegister.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/components/ButtonResgister.scss';
+
+const ButtonRegister = () => (
+  <button className='ButtonRegister' type='submit'>Resgístrate</button>
+);
+
+export default ButtonRegister;
