a8689a6c0121efe1e4c040728999bd80fb7ea903
fiorellachilcon <fiorellachilc_20@hotmail.com>
2020-08-21 22:34:18 -0500

    maquetizacion responsive de vista de bienvenida

--- src/containers/Welcome.jsx
+++ src/containers/Welcome.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/styles/components/Welcome.scss';
+import AwesomeSlider from 'react-awesome-slider';
 import ButtonRegister from '../components/ButtonRegister';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+import 'react-awesome-slider/dist/styles.css';
 
 const Welcome = () => (
   <>
-    <p>ESTAS EN WELCOME</p>
-    <Link to='/signup' style={{ textDecoration: 'none' }}>
-      <ButtonRegister namebutton='Registrate' />
-    </Link>
-    <Link to='/login' style={{ textDecoration: 'none' }}>
-      <ButtonRegister namebutton='Inicia sesion' />
-    </Link>
+    <div className='images-container'>
+      <AwesomeSlider animation='cubeAnimation'>
+        <div className='doula-images'>
+          <img src='src/assets/images/doula2.png' alt='img' />
+          <p>El embarazo es una etapa muy especial en la vida de las mujeres y queremos que lo disfrutes</p>
+        </div>
+        <div className='doula-images'>
+          <img src='src/assets/images/doula1.png' alt='img' />
+          <p>Únete a nuestra red de madres experimentadas, un espacio seguro y sororo</p>
+        </div>
+        <div className='doula-images'>
+          <img src='src/assets/images/doula3.png' alt='img' />
+          <p>Donde podras responder dudas y compartir tus experiencias y recomendaciones</p>
+        </div>
+      </AwesomeSlider>
+    </div>
+    <div className='options'>
+      <Link to='/signup' style={{ textDecoration: 'none' }}>
+        <ButtonRegister namebutton='Registrate' />
+      </Link>
+      <Link to='/login' style={{ textDecoration: 'none' }}>
+        <button type='button' className='buttonLogin'>Inicia sesión</button>
+      </Link>
+    </div>
   </>
 );
 
