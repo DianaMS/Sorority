904f88e6ef86c8424a4f6b3724f7bf2f3fa05c9b
DianaMS <dmartinezs@uni.pe>
2020-08-22 11:52:38 -0500

    importa componentes

--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Chat.scss';
 import perfil from '../assets/static/Mask-Perfil.png';
-
+import doctora from '../assets/static/medica8.png';
 import HeadBack from '../components/HeadBack';
 import NavigatorNav from '../components/NavigatorNav';
 import Chat3 from '../components/Chat3';
@@ -16,7 +16,7 @@ const Chat = () => (
         <div className='contenedorWindowChat'>
           <Chat3 photoProfile3={perfil} name3='Diana Martinez' lastMessage3='Gracias :)' />
           <Link to='/onechat' className='link-nav'>
-            <Chat3 photoProfile3={perfil} name3='Dra Rosa Rojas' lastMessage3='Sí! Listo :) Quedamos en contacto, un abrazo.' />
+            <Chat3 photoProfile3={doctora} name3='Dra Rosa Rojas' lastMessage3='Sí! Listo :) Quedamos en contacto, un abrazo.' />
           </Link>
         </div>
       </div>
